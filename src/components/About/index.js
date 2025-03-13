b3649821a6e6470c2a0e1e2a4d0551b24ae760e2
Kevin Kar-Ho Lo <kevinkarholo@gmail.com>
2025-03-13 17:25:54 -0400

    Added Home Picture and About Me bio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,7 +2,18 @@
 
 const About = () => {
     return (
-        <p>About Test</p>
+        <>
+            <h2>About Me</h2>
+            <p>I thrive on tackling challenges and adapting to new environments—whether it's optimizing strategies in board games or navigating life in a foreign country.
+                With a background in Engineering, I have a strong foundation in problem-solving, always looking for efficient and innovative solutions. 
+                My love for strategy extends beyond work, as I enjoy playing and min-maxing board games, refining my ability to think critically and anticipate outcomes.</p>
+            <p>Versatility is another defining trait of mine. I took a leap and moved to Japan for almost four years, knowing no one and speaking little of the language. 
+                I quickly immersed myself, learning Japanese, adapting to social etiquettes, and making it my second home. That experience reinforced my ability to adapt, 
+                learn on the fly, and thrive in unfamiliar situations.</p>
+            <p>Whether it’s solving technical challenges, optimizing strategies, or adapting to new cultures, 
+                I embrace change and approach every situation with curiosity and determination.</p>
+        </>
+
     )
 }
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,29 @@
-
+import styled from "styled-components";
 
 const Home = () => {
     return (
-        <h2>Home Test</h2>
+        <>
+            <TitleContainer>
+                <h2>Hello, <br/> I'm Kevin Lo</h2>
+                <StyledImg src="/assets/kevinPicture.jpg" alt="Picture of Kevin" />
+            </TitleContainer>
+            <p>I'm a Full-Stack Developer in training with a passion for making dreams into reality.</p>
+        </>
+
     )
 }
+
+const TitleContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+const StyledImg = styled.img`
+    margin: 0 1rem;
+    width: 100px;
+    height: 100px;
+    object-fit: cover;
+    border-radius: 50%;
+`
 
 export default Home
