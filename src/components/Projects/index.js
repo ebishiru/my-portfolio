fb599ab3d54eddc8836188c37ef1ff94d61aadfe
Kevin Kar-Ho Lo <kevinkarholo@gmail.com>
2025-03-14 13:49:58 -0400

    Added Icons for languages and colored them

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,11 +1,17 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHtml5, faCss3, faReact, faNodeJs } from "@fortawesome/free-brands-svg-icons";
 
 const Projects = () => {
     return (
         <>
         <h2>Languages and Frameworks I'm familiar with:</h2>
-        <ul></ul>
+        <IconContainer>
+            <li><StyledIcon icon={faHtml5} style={{ color: "#E34F26" }}></StyledIcon></li>
+            <li><StyledIcon icon={faCss3} style={{ color: "#1572B6" }}></StyledIcon></li>
+            <li><StyledIcon icon={faReact} style={{ color: "#61DAFB" }}></StyledIcon></li>
+            <li><StyledIcon icon={faNodeJs} style={{ color: "#339933" }}></StyledIcon></li>
+        </IconContainer>
         <h2>Projects I've worked on:</h2>
         <ProjectContainer>
             <a href="https://github.com/ebishiru/project-js-nyan-cat" target="_blank"><ProjectImg src="/assets/nyan-cat-picture.jpg" alt='Picture of the game "I CAN HAZ CHEEZBURGER?!??"'/></a>
@@ -14,6 +20,21 @@ const Projects = () => {
         </>
     )
 }
+
+const IconContainer = styled.ul`
+    display: flex;
+    justify-content: space-evenly;
+    list-style-type: none;
+    padding: 0 10vw;
+`
+
+const StyledIcon = styled(FontAwesomeIcon)`
+    font-size: 3rem;
+    
+    &:hover {
+        opacity: 0.7;
+    }
+`
 
 const ProjectContainer = styled.div`
     display: flex;
